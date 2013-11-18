372aa1067bfa34d9b7ca7c3ff88bee91a035aa2b
resteve <resteve@zikzakmedia.com>
2013-11-18 09:56:14 +0100

    Change setup.py to use a better structure

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 #This file is part account_payment_days module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 
 from setuptools import setup
 import re
+import os
 import ConfigParser
+
+MODULE = 'account_payment_days'
+PREFIX = 'trytonspain'
+MODULE2PREFIX = {}
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -21,28 +29,32 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonspain_account_payment_days',
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description='Account Payment Days',
-    author='NaN·tic',
-    author_email='info@NaN-tic.com',
-    url='http://www.nan-tic.com',
-    download_url="https://bitbucket.org/trytonspain/trytond-account_payment_days",
-    package_dir={'trytond.modules.account_payment_days': '.'},
+    description=('Tryton module account payment days'),
+    author='Zikzakmedia SL',
+    author_email='zikzak@zikzakmedia.com',
+    url='http://www.zikzakmedia.com',
+    download_url='https://bitbucket.org/trytonspain/trytond-%s' % MODULE,
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.account_payment_days',
-        'trytond.modules.account_payment_days.tests',
-    ],
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+        ],
     package_data={
-        'trytond.modules.account_payment_days': info.get('xml', []) \
-            + ['tryton.cfg', 'locale/*.po'],
-    },
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', ]),
+        },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -50,22 +62,22 @@ setup(name='trytonspain_account_payment_days',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
+        'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
         'Natural Language :: Catalan',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
-    ],
+        ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_payment_days = trytond.modules.account_payment_days
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
 )
