f2c524648392f82ab1dca5806de8946e5897db96
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-12-16 17:06:59 +0100

    Add compatibility with invoice_speedup patch (grafted from e0115b38ce5c1d5f57f779527db178ab2a4016f2)

--- invoice.py
+++ invoice.py
@@ -1,16 +1,16 @@
-#This file is part account_payment_days module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part account_payment_days module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from functools import wraps
 from trytond.model import ModelView, ModelSQL
 from trytond.transaction import Transaction
 
 __all__ = ['Invoice']
 
 
-class Invoice(ModelSQL, ModelView):
-    __name__ = 'account.invoice'
-
-    def create_move(self):
+def process_payment_days(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
         if self.type in ('out_invoice', 'out_credit_note'):
             payment_days = self.party.customer_payment_days
         else:
@@ -20,4 +20,20 @@ class Invoice(ModelSQL, ModelView):
             payment_days = [int(x) for x in payment_days.split()]
 
         with Transaction().set_context(account_payment_days=payment_days):
-            return super(Invoice, self).create_move()
+            return func(self, *args, **kwargs)
+    return wrapper
+
+
+class Invoice(ModelSQL, ModelView):
+    __name__ = 'account.invoice'
+
+    @process_payment_days
+    def create_move(self):
+        # This method is called when not issuing invoice_speedup.patch
+        # XXX: Remove when patch is commited in core and avoid decorator
+        return super(Invoice, self).create_move()
+
+    @process_payment_days
+    def get_move(self):
+        # This method is called when using invoice_speedup.patch
+        return super(Invoice, self).get_move()
