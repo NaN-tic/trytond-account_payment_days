26eb0d3a6bd4bab68bb5af8c7b805c516b70f876
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-12 09:43:50 +0200

    Update tests

--- tests/test_account_payment_days.py
+++ tests/test_account_payment_days.py
@@ -3,11 +3,10 @@
 # copyright notices and license terms.
 from decimal import Decimal
 import datetime
-import doctest
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
 
 
@@ -15,74 +14,72 @@ class AccountPaymentDaysTestCase(ModuleTestCase):
     'Test Account Payment Days module'
     module = 'account_payment_days'
 
-    def setUp(self):
-        super(AccountPaymentDaysTestCase, self).setUp()
-        self.payment_term = POOL.get('account.invoice.payment_term')
-        self.currency = POOL.get('currency.currency')
-
+    @with_transaction()
     def test0010payment_term(self):
         'Test payment_term'
+        pool = Pool()
+        PaymentTerm = pool.get('account.invoice.payment_term')
+        Currency = pool.get('currency.currency')
         payment_days = [5, 20]
-        with Transaction().start(DB_NAME, USER, context=CONTEXT) as tx:
-            with tx.set_context(account_payment_days=payment_days):
-                currency, = self.currency.create([{
-                            'name': 'cu1',
-                            'symbol': 'cu1',
-                            'code': 'cu1'
-                            }])
+        currency, = Currency.create([{
+                    'name': 'cu1',
+                    'symbol': 'cu1',
+                    'code': 'cu1'
+                    }])
 
-                term, = self.payment_term.create([{
-                            'name': '30 days, 1 month, 1 month + 15 days',
-                            'lines': [
-                                ('create', [{
-                                            'sequence': 0,
-                                            'type': 'percent_on_total',
-                                            'divisor': 4,
-                                            'percentage': 25,
-                                            'relativedeltas': [
-                                                ('create', [{
-                                                            'days': 30,
-                                                            }])],
-                                            }, {
-                                            'sequence': 1,
-                                            'type': 'percent_on_total',
-                                            'divisor': 4,
-                                            'percentage': 25,
-                                            'relativedeltas': [
-                                                ('create', [{
-                                                            'months': 1,
-                                                            }])],
-                                            }, {
-                                            'sequence': 2,
-                                            'type': 'fixed',
-                                            'amount': Decimal('396.84'),
-                                            'currency': currency.id,
-                                            'relativedeltas': [
-                                                ('create', [{
-                                                            'months': 1,
-                                                            'days': 30,
-                                                            }])],
-                                            }, {
-                                            'sequence': 3,
-                                            'type': 'remainder',
-                                            'relativedeltas': [
-                                                ('create', [{
-                                                            'months': 2,
-                                                            'days': 30,
-                                                            'day': 15,
-                                                            }])],
-                                            },
-                                        ])],
-                            }])
+        term, = PaymentTerm.create([{
+                    'name': '30 days, 1 month, 1 month + 15 days',
+                    'lines': [
+                        ('create', [{
+                                    'sequence': 0,
+                                    'type': 'percent_on_total',
+                                    'divisor': 4,
+                                    'ratio': Decimal('.25'),
+                                    'relativedeltas': [
+                                        ('create', [{
+                                                    'days': 30,
+                                                    }])],
+                                    }, {
+                                    'sequence': 1,
+                                    'type': 'percent_on_total',
+                                    'divisor': 4,
+                                    'ratio': Decimal('.25'),
+                                    'relativedeltas': [
+                                        ('create', [{
+                                                    'months': 1,
+                                                    }])],
+                                    }, {
+                                    'sequence': 2,
+                                    'type': 'fixed',
+                                    'amount': Decimal('396.84'),
+                                    'currency': currency.id,
+                                    'relativedeltas': [
+                                        ('create', [{
+                                                    'months': 1,
+                                                    'days': 30,
+                                                    }])],
+                                    }, {
+                                    'sequence': 3,
+                                    'type': 'remainder',
+                                    'relativedeltas': [
+                                        ('create', [{
+                                                    'months': 2,
+                                                    'days': 30,
+                                                    'day': 15,
+                                                    }])],
+                                    },
+                                ])],
+                    }])
 
-                terms = term.compute(Decimal('1587.35'), currency,
-                        date=datetime.date(2011, 10, 1))
-                self.assertEqual(terms, [
-                        (datetime.date(2011, 11, 5), Decimal('396.84')),
-                        (datetime.date(2011, 11, 5), Decimal('396.84')),
-                        (datetime.date(2011, 12, 5), Decimal('396.84')),
-                        (datetime.date(2012, 01, 20), Decimal('396.83')),
-                        ])
+        with Transaction().set_context(account_payment_days=payment_days):
+            terms = term.compute(Decimal('1587.35'), currency,
+                    date=datetime.date(2011, 10, 1))
+            self.assertEqual(terms, [
+                    (datetime.date(2011, 11, 5), Decimal('396.84')),
+                    (datetime.date(2011, 11, 5), Decimal('396.84')),
+                    (datetime.date(2011, 12, 5), Decimal('396.84')),
+                    (datetime.date(2012, 01, 20), Decimal('396.83')),
+                    ])
 
 
 def suite():
